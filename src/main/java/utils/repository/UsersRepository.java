7c6a1128716995c88577a5fe9a629f94e51e6b35
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-17 23:54:34 +0200

    Adding abstract method get users by name and id no

--- src/main/java/utils/repository/UsersRepository.java
+++ src/main/java/utils/repository/UsersRepository.java
@@ -4,8 +4,12 @@ import entities.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface UsersRepository extends JpaRepository<UsersEntity,Long> {
 
+    List<UsersEntity> findByUsersIdentityNo(Long usersIdentityNo);
 
+    List<UsersEntity> findByUsersFullName(String usersFullName);
 }
