b30ad783f699d51e9bb28fa10f58b79c5c78ded9
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-23 19:28:28 +0200

    Adding privileges entity cases and it's field

---
+++ src/main/java/com/aa/AA/entities/PrivilegeEntity.java
@@ -0,0 +1,28 @@
+package com.aa.AA.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@ToString
+
+@NoArgsConstructor
+public class PrivilegeEntity {
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int pkPrivilegeId;
+    @Setter
+    @Getter
+    private String privilegeName;
+    /*
+        This byte will be used to determine whether privilege is ready to used
+     */
+    @Setter
+    @Getter
+    private byte privilegeStatus;
+
+}
