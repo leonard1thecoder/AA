e3fb19fc16719b8b2e115004348ab54b0391341d
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-24 12:34:26 +0200

    Adding Jpa Repository for LiquorStorePromotionUsersRepository

---
+++ src/main/java/com/aa/AA/utils/repository/LiquorStorePromotionUsersRepository.java
@@ -0,0 +1,6 @@
+package com.aa.AA.utils.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LiquorStorePromotionUsersRepository extends JpaRepository {
+}
