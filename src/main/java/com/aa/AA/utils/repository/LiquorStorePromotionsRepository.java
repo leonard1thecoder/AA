b11e43bc8de19fddb65b0b564532406a85e17890
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-24 12:56:14 +0200

    Adding Component Annotation

--- src/main/java/com/aa/AA/utils/repository/LiquorStorePromotionsRepository.java
+++ src/main/java/com/aa/AA/utils/repository/LiquorStorePromotionsRepository.java
@@ -1,6 +1,8 @@
 package com.aa.AA.utils.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface LiquorStorePromotionsRepository extends JpaRepository {
 }
--- src/main/java/com/aa/AA/utils/repository/LiquorStoreStockSizeRepository.java
+++ src/main/java/com/aa/AA/utils/repository/LiquorStoreStockSizeRepository.java
@@ -2,6 +2,9 @@ package com.aa.AA.utils.repository;
 
 import com.aa.AA.entities.LiquorStoreStockSizeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 
 public interface LiquorStoreStockSizeRepository extends JpaRepository<LiquorStoreStockSizeEntity,Short> {
 }
