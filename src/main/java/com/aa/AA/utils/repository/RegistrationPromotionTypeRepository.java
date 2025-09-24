a3541e01fa06635be510410e55026fef37886a6b
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-24 13:55:00 +0200

    checking class name in generic type hence class name changed

--- src/main/java/com/aa/AA/utils/repository/RegistrationPromotionTypeRepository.java
+++ src/main/java/com/aa/AA/utils/repository/RegistrationPromotionTypeRepository.java
@@ -1,9 +1,9 @@
 package com.aa.AA.utils.repository;
 
-import com.aa.AA.entities.RegistrationPromotionType;
+import com.aa.AA.entities.RegistrationPromotionTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface RegistrationPromotionTypeRepository extends JpaRepository<RegistrationPromotionType,Byte> {
+public interface RegistrationPromotionTypeRepository extends JpaRepository<RegistrationPromotionTypeEntity,Byte> {
 }
