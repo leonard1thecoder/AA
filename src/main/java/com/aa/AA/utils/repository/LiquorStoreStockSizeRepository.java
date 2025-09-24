14dce033a3bb03368b190e578e2690be5aec0b13
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-24 10:27:19 +0200

    Adding repository for Stock and Stock Size

---
+++ src/main/java/com/aa/AA/utils/repository/LiquorStoreStockSizeRepository.java
@@ -0,0 +1,7 @@
+package com.aa.AA.utils.repository;
+
+import com.aa.AA.entities.LiquorStoreStockSizeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LiquorStoreStockSizeRepository extends JpaRepository<LiquorStoreStockSizeEntity,Short> {
+}
