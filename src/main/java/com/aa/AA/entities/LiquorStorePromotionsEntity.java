0d8cfcd216b2cbb0fcea0c41f02ce2a29b5b9110
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-25 09:04:38 +0200

    amended

--- src/main/java/com/aa/AA/entities/LiquorStorePromotionsEntity.java
+++ src/main/java/com/aa/AA/entities/LiquorStorePromotionsEntity.java
@@ -12,6 +12,15 @@ import java.util.List;
 @ToString
 @NoArgsConstructor
 @Entity
+
+
+/*
+Promotion need to filter by event sTE
+    * No of purchase to certain product, get certain percentage discount
+    * First Number of people to select upcoming Artist
+    * First Number of people to select to come in event and come !!! People will promotion in event
+    *
+ * */
 public class LiquorStorePromotionsEntity {
 
     @Id
@@ -22,13 +31,21 @@ public class LiquorStorePromotionsEntity {
     @ManyToMany
     private LiquorStoreEntity entity;
 
-    @OneToMany(mappedBy = "liquorStorePromotionsEntities")
+    @JoinColumn
+    @OneToOne
+    private LiquorStorePromotionsTypeEntity liquorStorePromotionsTypeEntity;
+
+    @OneToMany(mappedBy = "liquorStorePromotionsEntities",cascade = CascadeType.ALL)
     private List<LiquorStorePromotionUsersEntity> liquorStorePromotionUsersEntities;
 
-    private String promotionName;
+    @OneToMany(mappedBy = "liquorStorePromotionsEntity",cascade = CascadeType.ALL)
+    private  List<UsersEntity> usersEntities;
+
+    private String promotionName, promotionDescription;
 
     private LocalDateTime promotionCreationDate, promotionExpirationDate;
 
-    private Byte promotionStatus;
+
+    private Byte numberOfPromotion,promotionStatus;
 
 }
