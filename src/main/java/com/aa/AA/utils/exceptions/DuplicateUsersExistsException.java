0207d028ef002cc9bd6ade5c66e3a8e250a581e4
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-11 16:36:02 +0200

    Adding exception for duplicate use in db

---
+++ src/main/java/com/aa/AA/utils/exceptions/DuplicateUsersExistsException.java
@@ -0,0 +1,7 @@
+package com.aa.AA.utils.exceptions;
+
+public class DuplicateUsersExistsException extends RuntimeException {
+    public DuplicateUsersExistsException(String message) {
+        super(message);
+    }
+}
