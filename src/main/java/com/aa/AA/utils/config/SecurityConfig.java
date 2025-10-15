9055bce1c9826e376d445445c7b93a529d2d5647
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-15 11:17:26 +0200

    Enabling method security and choose login method bt http security

--- src/main/java/com/aa/AA/utils/config/SecurityConfig.java
+++ src/main/java/com/aa/AA/utils/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final Filter jwtAuthFilterConfig;
@@ -38,7 +40,9 @@ public class SecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authencticationProvider)
-                .addFilterBefore(jwtAuthFilterConfig, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilterConfig, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 
