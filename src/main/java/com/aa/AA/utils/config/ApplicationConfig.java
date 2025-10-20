929db3d477b66a5ac3636f863abee29eb1276441
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-20 22:19:42 +0200

    Removing @EnableCache annotation to Redis service class

--- src/main/java/com/aa/AA/utils/config/ApplicationConfig.java
+++ src/main/java/com/aa/AA/utils/config/ApplicationConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableCaching
 
 public class ApplicationConfig {
 
