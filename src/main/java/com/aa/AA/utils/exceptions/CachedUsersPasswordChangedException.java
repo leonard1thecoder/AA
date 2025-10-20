8716d8504c287542685d1885215a3a7630c8f7f7
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-20 22:20:12 +0200

    Creating Exception

---
+++ src/main/java/com/aa/AA/utils/exceptions/CachedUsersPasswordChangedException.java
@@ -0,0 +1,7 @@
+package com.aa.AA.utils.exceptions;
+
+public class CachedUsersPasswordChangedException extends RuntimeException {
+    public CachedUsersPasswordChangedException(String message) {
+        super(message);
+    }
+}
