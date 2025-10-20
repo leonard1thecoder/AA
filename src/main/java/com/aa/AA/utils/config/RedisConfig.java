af6b80db9ef5df550ca05999eb5d62ec0b105361
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-20 22:21:41 +0200

    CreatingRedisConfig class for caching

---
+++ src/main/java/com/aa/AA/utils/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.aa.AA.utils.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
+     RedisTemplate redisTemplate = new RedisTemplate<>();
+     redisTemplate.setConnectionFactory(connectionFactory);
+     redisTemplate.setKeySerializer(new StringRedisSerializer());
+     redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+     return redisTemplate;
+    }
+
+}
