e7b82af573a8c6148f4db6305dd5181c0a59bd75
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-22 01:00:04 +0200

    Extending RuntimeException class

--- src/main/java/com/aa/AA/utils/exceptions/ServiceTimeoutException.java
+++ src/main/java/com/aa/AA/utils/exceptions/ServiceTimeoutException.java
@@ -2,7 +2,7 @@ package com.aa.AA.utils.exceptions;
 
 import java.util.concurrent.*;
 
-public class ServiceTimeoutException extends TimeoutException {
+public class ServiceTimeoutException extends RuntimeException {
     public ServiceTimeoutException(String message) {
         super(message);
     }
