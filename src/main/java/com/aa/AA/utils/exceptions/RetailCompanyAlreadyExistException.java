d1678cb8b0f8028ccfab53727813addabf7c7618
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-30 00:24:46 +0200

    Creating exception when retail company is not found when searching from db

---
+++ src/main/java/com/aa/AA/utils/exceptions/RetailCompanyAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.aa.AA.utils.exceptions;
+
+public class RetailCompanyAlreadyExistException extends RuntimeException {
+    public RetailCompanyAlreadyExistException(String message) {
+        super(message);
+    }
+}
