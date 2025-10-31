7ffe976dd9695e5f769f9cc1b5151d1a81689187
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-10-31 16:52:27 +0200

    added required modules and open packages to use by spring

--- users_microservice/src/main/java/module-info.java
+++ users_microservice/src/main/java/module-info.java
@@ -4,5 +4,13 @@ module users.microservice {
     requires static lombok;
     requires jakarta.persistence;
     requires spring.security.core;
+    requires privileges.microservice;
+    requires spring.data.jpa;
+    requires spring.context;
+    requires org.hibernate.orm.core;
+    requires spring.beans;
 
+    opens com.users.application.entities;
+    opens com.users.application;
+    opens com.users.application.repository;
 }
