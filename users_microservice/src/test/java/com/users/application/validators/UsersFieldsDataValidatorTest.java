0f0ebc8f9d283045f56b469371ecfb23bf0d0d9b
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-11-14 09:41:14 +0200

    create test class for data validator

---
+++ users_microservice/src/test/java/com/users/application/validators/UsersFieldsDataValidatorTest.java
@@ -0,0 +1,16 @@
+package com.users.application.validators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersFieldsDataValidatorTest {
+
+    @Test
+    void validateIdentityNo() {
+    }
+
+    @Test
+    void validateCellphoneNo() {
+    }
+}
