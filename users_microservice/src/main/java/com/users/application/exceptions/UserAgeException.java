58cbb7544c8061f5f918af322a5bf3ae7a7d2f22
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-11-16 18:46:49 +0200

    used validator class for  cellphone number, age and identity number

---
+++ users_microservice/src/main/java/com/users/application/exceptions/UserAgeException.java
@@ -0,0 +1,7 @@
+package com.users.application.exceptions;
+
+public class UserAgeException extends RuntimeException {
+    public UserAgeException(String message) {
+        super(message);
+    }
+}
--- users_microservice/src/main/java/com/users/application/exceptions/UserUnderAgeOf18Exception.java
+++
@@ -1,7 +0,0 @@
-package com.users.application.exceptions;
-
-public class UserUnderAgeOf18Exception extends RuntimeException {
-    public UserUnderAgeOf18Exception(String message) {
-        super(message);
-    }
-}
