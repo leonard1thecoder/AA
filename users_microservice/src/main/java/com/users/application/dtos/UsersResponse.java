fcf14232abdd4261740280adddf8e189f64d1845
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-11-21 03:50:51 +0200

    Changed user entity field and also changed here in service

--- users_microservice/src/main/java/com/users/application/dtos/UsersResponse.java
+++ users_microservice/src/main/java/com/users/application/dtos/UsersResponse.java
@@ -2,18 +2,23 @@ package com.users.application.dtos;
 
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.privileges.application.entity.Privileges;
 import com.utils.application.ResponseContract;
 import lombok.*;
+import org.springframework.stereotype.Component;
 
 
-
-@ToString
+@Data
+@Builder
+@AllArgsConstructor
+@Component
 public class UsersResponse implements ResponseContract {
     private Long id;
+    private Privileges privileges;
     private String usersIdentityNo;
     private Short usersStatus,usersAge;
-    private String usersFullName, usersEmailAddress, usersCountryName, usersRegistrationDate, usersLanguage, usersModifiedDate;
-    private String token;
+    private String usersFullName,cellphoneNo, usersEmailAddress, usersRegistrationDate, usersModifiedDate;
+
 
     @JsonCreator
     public UsersResponse(){}
@@ -24,90 +29,11 @@ public class UsersResponse implements ResponseContract {
         this.usersFullName = usersFullName;
         this.usersEmailAddress = usersEmailAddress;
         this.usersRegistrationDate = usersRegistrationDate;
-        this.usersCountryName = usersCountryName;
-        this.usersLanguage = usersLanguage;
+
         this.usersModifiedDate = usersModifiedDate;
     }
 
-    public String getUsersIdentityNo() {
-        return usersIdentityNo;
-    }
 
-    public void setUsersIdentityNo(String usersIdentityNo) {
-        this.usersIdentityNo = usersIdentityNo;
-    }
-
-    public String getUsersFullName() {
-        return usersFullName;
-    }
-
-    public void setUsersFullName(String usersFullName) {
-        this.usersFullName = usersFullName;
-    }
-
-    public Short getUsersAge() {
-        return usersAge;
-    }
-
-    public void setUsersAge(Short usersAge) {
-        this.usersAge = usersAge;
-    }
-
-    public Short getUsersStatus() {
-        return usersStatus;
-    }
-
-    public void setUsersStatus(Short usersStatus) {
-        this.usersStatus = usersStatus;
-    }
-
-    public String getUsersEmailAddress() {
-        return usersEmailAddress;
-    }
-
-    public void setUsersEmailAddress(String usersEmailAddress) {
-        this.usersEmailAddress = usersEmailAddress;
-    }
-
-    public String getUsersCountryName() {
-        return usersCountryName;
-    }
-
-    public void setUsersCountryName(String usersCountryName) {
-        this.usersCountryName = usersCountryName;
-    }
-
-    public String getUsersRegistrationDate() {
-        return usersRegistrationDate;
-    }
-
-    public void setUsersRegistrationDate(String usersRegistrationDate) {
-        this.usersRegistrationDate = usersRegistrationDate;
-    }
-
-    public String getUsersLanguage() {
-        return usersLanguage;
-    }
-
-    public void setUsersLanguage(String usersLanguage) {
-        this.usersLanguage = usersLanguage;
-    }
-
-    public String getUsersModifiedDate() {
-        return usersModifiedDate;
-    }
-
-    public void setUsersModifiedDate(String usersModifiedDate) {
-        this.usersModifiedDate = usersModifiedDate;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 
     @Override
     public Long getId() {
