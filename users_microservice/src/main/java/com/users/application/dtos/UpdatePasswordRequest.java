aeee854b8efcddc8e25ddedf05c4c7903b2c4e88
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-05 12:12:47 +0200

    removing boiler code

--- users_microservice/src/main/java/com/users/application/dtos/UpdatePasswordRequest.java
+++ users_microservice/src/main/java/com/users/application/dtos/UpdatePasswordRequest.java
@@ -1,43 +1,17 @@
 package com.users.application.dtos;
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
+
+@Data
+@Builder
+@AllArgsConstructor
 public class UpdatePasswordRequest {
 
 
-    @Setter
     private String usersPassword,usersEmailAddress;
-    @Setter
     private String usersConfirmPassword;
 
-
-    public String getUsersEmailAddress() {
-        return usersEmailAddress;
-    }
-
-    public void setUsersEmailAddress(String usersEmailAddress) {
-        this.usersEmailAddress = usersEmailAddress;
-    }
-
-    public String getUsersConfirmPassword() {
-        return usersConfirmPassword;
-    }
-
-    public String getUsersPassword() {
-        return usersPassword;
-    }
-
-    public UpdatePasswordRequest(String usersPassword, String usersEmailAddress, String usersConfirmPassword) {
-        this.usersPassword = usersPassword;
-        this.usersEmailAddress = usersEmailAddress;
-        this.usersConfirmPassword = usersConfirmPassword;
-    }
-
-    public void setUsersPassword(String usersPassword) {
-        this.usersPassword = usersPassword;
-    }
-
-    public void setUsersConfirmPassword(String usersConfirmPassword) {
-        this.usersConfirmPassword = usersConfirmPassword;
-    }
 }
