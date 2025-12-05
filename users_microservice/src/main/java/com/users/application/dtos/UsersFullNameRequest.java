6348e20449a43edb59ea957be2b18774f3972f77
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-05 12:12:27 +0200

    removing boiler code

--- users_microservice/src/main/java/com/users/application/dtos/UsersFullNameRequest.java
+++ users_microservice/src/main/java/com/users/application/dtos/UsersFullNameRequest.java
@@ -1,18 +1,13 @@
 package com.users.application.dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class UsersFullNameRequest {
     private String usersFullName;
-
-    public UsersFullNameRequest(String usersFullName) {
-        this.usersFullName = usersFullName;
-    }
-
-    public String usersFullName() {
-        return usersFullName;
-    }
-
-    public UsersFullNameRequest setUsersFullName(String usersFullName) {
-        this.usersFullName = usersFullName;
-        return this;
-    }
 }
