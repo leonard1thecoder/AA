f4fa91d65044a475f2594e32d0b906ba2451d7d9
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-17 17:28:44 +0200

    Jwt is in Caps not lowercase 'Bearer' not 'bearer'
    and add logs

--- users_microservice/src/main/java/com/users/application/configurations/JwtAuthFilterConfig.java
+++ users_microservice/src/main/java/com/users/application/configurations/JwtAuthFilterConfig.java
@@ -6,6 +6,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,13 +21,15 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthFilterConfig extends OncePerRequestFilter {
-
+private final static Logger logger  = LoggerFactory.getLogger(JwtAuthFilterConfig.class);
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
     public JwtAuthFilterConfig(JwtService jwtService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        logger.info("jwt service : {}, userDetails : {}", jwtService,userDetailsService);
+
     }
 
     @Override
@@ -34,17 +38,20 @@ public class JwtAuthFilterConfig extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
             final var authHeader = request.getHeader("authorization");
+           logger.info("token from header : {}", authHeader);
             final String jwt;
             final String userEmailAddress; // todo
-            if (authHeader == null || !authHeader.startsWith("bearer ")){
+            if (authHeader == null || !authHeader.startsWith("Bearer ")){
                 filterChain.doFilter(request,response);
                 return;
             }
+
             jwt = authHeader.substring(7);
             userEmailAddress = jwtService.extractUsername(jwt);
+        logger.info("jwt config  : {}", jwt);
 
             if(userEmailAddress != null && SecurityContextHolder.getContext().getAuthentication() == null){
-
+                logger.info("email config  : {}", userEmailAddress);
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmailAddress);
                 if(jwtService.isTokenValid(jwt,userDetails)){
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
