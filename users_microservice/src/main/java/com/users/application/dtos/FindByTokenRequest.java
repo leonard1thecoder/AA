cecf9ed0b20a3d0e3b4782605609ab83b5f4ca5a
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-19 18:24:44 +0200

    FindByTokenRequest to verify token

---
+++ users_microservice/src/main/java/com/users/application/dtos/FindByTokenRequest.java
@@ -0,0 +1,14 @@
+package com.users.application.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class FindByTokenRequest {
+    private String token;
+}
+
