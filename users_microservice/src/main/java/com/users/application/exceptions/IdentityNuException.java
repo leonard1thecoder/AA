e49f95b920a2dcea7210afa6de49b5c4d91cd377
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-21 16:59:11 +0200

    Adding exception for validate identity nu

---
+++ users_microservice/src/main/java/com/users/application/exceptions/IdentityNuException.java
@@ -0,0 +1,7 @@
+package com.users.application.exceptions;
+
+public class IdentityNuException extends RuntimeException {
+    public IdentityNuException(String message) {
+        super(message);
+    }
+}
