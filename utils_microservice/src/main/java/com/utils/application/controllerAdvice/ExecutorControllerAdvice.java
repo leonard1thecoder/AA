35a02daf66d76fccc1344a515aecee4565d866ea
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-12-30 16:37:58 +0200

    Adding  logs for advise

--- utils_microservice/src/main/java/com/utils/application/controllerAdvice/ExecutorControllerAdvice.java
+++ utils_microservice/src/main/java/com/utils/application/controllerAdvice/ExecutorControllerAdvice.java
@@ -5,19 +5,26 @@ import com.utils.application.globalExceptions.errorResponse.*;
 
 import com.utils.application.ExceptionHandlerReporter;
 import com.utils.application.globalExceptions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+
 @ControllerAdvice
 public class ExecutorControllerAdvice extends ExceptionHandlerReporter {
+    private static final Logger logger = LoggerFactory.getLogger(ExecutorControllerAdvice.class);
 
 
 
     @ExceptionHandler(ServiceTimeoutException.class)
-    public ResponseEntity<ErrorResponse> manageServiceTimeoutException(){
-        return new ResponseEntity<>(new ErrorResponse(getIssueDateFormatted(),getResolveIssueDetails(), getMessage()),HttpStatus.GATEWAY_TIMEOUT);
+    public ResponseEntity<List<ErrorResponse>> manageServiceTimeoutException(){
+        var list = List.of(new ErrorResponse(getIssueDateFormatted(),getResolveIssueDetails(), getMessage()));
+        logger.warn("Error response : {}, error code : {}", list,HttpStatus.FORBIDDEN.value());
+        return new ResponseEntity<>(list, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(ServiceInterruptedException.class)
