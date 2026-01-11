85752feb657e47fc170d067040a1abed259d3a91
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-01-11 23:17:53 +0200

    permitting / endpoint

--- users_microservice/src/main/java/com/users/application/configurations/SecurityConfig.java
+++ users_microservice/src/main/java/com/users/application/configurations/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -33,12 +34,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf
-                        -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth
                         -> auth.requestMatchers("/dev/api/auth/**")
                         .permitAll()
                         .requestMatchers("/dev/api/verify/**")
+                        .permitAll()
+                        .requestMatchers("/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(
