79c1aa1d09aab474c4e098ca8b8bb40698ba7b65
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-01-20 00:42:39 +0200

    Adding method to return error response to controller instead od throwing exception

--- utils_microservice/src/main/java/com/utils/application/ExceptionHandler.java
+++ utils_microservice/src/main/java/com/utils/application/ExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.utils.application;
 
+import com.utils.application.globalExceptions.errorResponse.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.utils.application.ExceptionHandlerReporter.formatDateTime;
 
@@ -21,5 +23,30 @@ public class ExceptionHandler {
             logger.warn("Error thrown by exception : {}\n Time thrown : {}\n Trace exception :{}", ex,formatDateTime(LocalDateTime.now()) , Arrays.stream(reportException.getStackTrace()).toList());
             return reportException;
         }
+    }
+
+    public static List<ErrorResponse> returnErrorResponse(boolean logging,RuntimeException ex, String errorMessage, String resolveIssueDetails){
+        try{
+            ExceptionHandlerReporter.setMessage(errorMessage);
+            ExceptionHandlerReporter.setResolveIssueDetails(resolveIssueDetails);
+            throw ex;
+        }catch (RuntimeException reportException){
+            if(logging)
+                logger.warn("Error thrown by exception : {}\n Time thrown : {}\n Trace exception :{}", ex,formatDateTime(LocalDateTime.now()) , Arrays.stream(reportException.getStackTrace()).toList());
+
+            return List.of(new ErrorResponse(errorMessage,formatDateTime(),resolveIssueDetails));
+        }
+    }
+
+    public static List<ErrorResponse> returnErrorResponse(boolean logging, String errorMessage, String resolveIssueDetails){
+
+            ExceptionHandlerReporter.setMessage(errorMessage);
+            ExceptionHandlerReporter.setResolveIssueDetails(resolveIssueDetails);
+
+            if(logging)
+                logger.warn("Exception trhown by controller advicer exception details ");
+
+            return List.of(new ErrorResponse(errorMessage,formatDateTime(),resolveIssueDetails));
+
     }
 }
