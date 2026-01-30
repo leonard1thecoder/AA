09564209f4fab3889a6d1ae992c99010ccbdf85f
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-01-30 03:34:00 +0200

    adding properies for building user-microservice docker image

--- utils_microservice/src/main/java/com/utils/application/configurations/RedisConfig.java
+++ utils_microservice/src/main/java/com/utils/application/configurations/RedisConfig.java
@@ -2,10 +2,12 @@ package com.utils.application.configurations;
 
 
 import com.utils.application.RedisService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -14,12 +16,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
+    public LettuceConnectionFactory redisConnectionFactory(
+            ) {
+        return new LettuceConnectionFactory("redis", 6379);
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(LettuceConnectionFactory connectionFactory){
      RedisTemplate redisTemplate = new RedisTemplate<>();
      redisTemplate.setConnectionFactory(connectionFactory);
      redisTemplate.setKeySerializer(new StringRedisSerializer());
      redisTemplate.setValueSerializer(new StringRedisSerializer());
-
+        
      return redisTemplate;
     }
 
