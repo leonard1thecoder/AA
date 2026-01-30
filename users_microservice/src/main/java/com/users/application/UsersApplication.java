15d6d70fd5c47d22dc102e59e2c2b3cf5439907b
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-01-30 03:49:34 +0200

    adding properies for building user-microservice docker image

--- users_microservice/src/main/java/com/users/application/UsersApplication.java
+++ users_microservice/src/main/java/com/users/application/UsersApplication.java
@@ -3,14 +3,14 @@ package com.users.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.docker.compose.lifecycle.DockerComposeProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-
 @EnableAsync
 @EnableJpaRepositories(
-        basePackages = {"com.privileges.application.repository",
+        basePackages = {
                 "com.users.application.repository"})
 @EntityScan({"com.privileges.application.entity","com.users.application.entities"})
 
@@ -19,11 +19,16 @@ import org.springframework.scheduling.annotation.EnableAsync;
         ,"com.users.application.services"
         ,"com.users.application.controllers"
         ,"com.users.application.configurations"
-        ,"com.users.application.mappers"
+        ,"com.users.application.mappers",
+        "com.utils.application",
+        "com.utils.application.configurations"
+        
+        
 } )
 @SpringBootApplication
 public class UsersApplication {
     public static void main(String[] argv) {
+        System.out.println("Developer name: Sizolwakhe Leonard Mthimunye \nEmail address : leonard1thecoder@gmail.com \nGithub repository : https://github.com/leonard1thecoder/AA");
         SpringApplication.run(UsersApplication.class, argv);
     }
 
