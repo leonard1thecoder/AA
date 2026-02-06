276d782552a427653219eb87715708a4cc953a1b
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-06 09:37:48 +0200

    managing controllers

--- retail_companies_microservice/src/main/java/com/retails/application/controller/RetailCompanyController.java
+++ retail_companies_microservice/src/main/java/com/retails/application/controller/RetailCompanyController.java
@@ -1,14 +1,16 @@
 package com.retails.application.controller;
 
-
 import com.retails.application.dto.*;
-import com.retails.application.mapper.RetailCompanyMapper;
+import com.retails.application.executor.RetailCompanyExecutor;
 import com.retails.application.service.RetailCompanyService;
 import com.utils.application.ResponseContract;
-import com.users.application.executor.ServiceConcurrentExecutor;
+import com.utils.application.globalExceptions.errorResponse.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -18,55 +20,88 @@ import static com.retails.application.service.RetailCompanyService.serviceHandle
 @RestController
 @RequestMapping("dev/retail/v1")
 public class RetailCompanyController {
+   private Logger logger = LoggerFactory.getLogger(RetailCompanyController.class);
 
-    RetailCompanyMapper retailCompanyMapper;
     RetailCompanyService retailCompanyService;
-    ServiceConcurrentExecutor serviceConcurrentExecutor;
+    RetailCompanyExecutor serviceConcurrentExecutor;
 
     @Autowired
-    public RetailCompanyController(@Autowired RetailCompanyMapper retailCompanyMapper,@Autowired RetailCompanyService retailCompanyService,@Autowired ServiceConcurrentExecutor serviceConcurrentExecutor) {
-        this.retailCompanyMapper = retailCompanyMapper;
+    public RetailCompanyController(@Autowired RetailCompanyService retailCompanyService) {
         this.retailCompanyService = retailCompanyService;
-        this.serviceConcurrentExecutor = serviceConcurrentExecutor;
+        this.serviceConcurrentExecutor = RetailCompanyExecutor.getInstance();
     }
-
     @PostMapping
-    public ResponseEntity<List<? extends ResponseContract>> registerRetailCompany(@RequestBody RegisterRetailCompanyRequest request){
+    public ResponseEntity<List<? extends ResponseContract>> registerRetailCompany(@RequestBody RegisterRetailCompanyRequest request, UriComponentsBuilder uriBuilder) {
         serviceHandler = "registerRetailCompany";
-        retailCompanyService.setRegisterLiquorStoreRequest(request);
+        retailCompanyService.setRegisterRetailCompanyRequest(request);
         var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
-        return  ResponseEntity.ok(response);
+
+        if (response.get(0) instanceof RetailCompanyResponse) {
+            var uri = uriBuilder.path("dev/retail/v1/getRetailCompanyById/{id}").buildAndExpand(request.getRetailCompanyName()).toUri();
+            return ResponseEntity.created(uri).body(response);
+        } else {
+            ErrorResponse error = (ErrorResponse) response;
+            logger.warn("Error response : {}", error);
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 
-    @GetMapping
-    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompaniesByOwnerName(@RequestBody DisplayRetailCompaniesByOwnerNameRequest request){
-        serviceHandler = "displayRetailCompaniesByOwnerName";
-        retailCompanyService.setDisplayRetailCompaniesByOwnerNameRequest(request);
-        var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
-        return  ResponseEntity.ok(response);
-    }
+//    @GetMapping
+//    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompaniesByOwnerName(@PathVariable DisplayRetailCompaniesByOwnerNameRequest request) {
+//        serviceHandler = "displayRetailCompaniesByOwnerName";
+//        retailCompanyService.setDisplayRetailCompaniesByOwnerNameRequest(request);
+//        var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
+//        if (response.get(0) instanceof RetailCompanyResponse)
+//            return ResponseEntity.ok(response);
+//        else{
+//            ErrorResponse error = (ErrorResponse) response;
+//            logger.warn("Error response : {}", error);
+//            return ResponseEntity.badRequest().body(List.of(error));
+//        }
+//    }
 
-    @GetMapping
-    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompanyByRetailCoRegNo(@RequestBody DisplayRetailCompanyByRetailCoRegNoRequest request){
+    @GetMapping("/getRetailCompanyById/{id}")
+    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompanyById(@PathVariable Long id) {
         serviceHandler = "displayRetailCompanyByRetailCoRegNo";
-        retailCompanyService.setDisplayRetailCompanyByRetailCoRegNoRequest(request);
+        retailCompanyService.setDisplayRetailCompanyByIdRequest(DisplayRetailCompanyByIdRequest
+                .builder()
+                .retailCompanyId(id)
+                .build());
         var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
-        return  ResponseEntity.ok(response);
+        if (response.get(0) instanceof RetailCompanyResponse)
+            return ResponseEntity.ok(response);
+        else {
+            ErrorResponse error = (ErrorResponse) response;
+            logger.warn("Error response : {}", error);
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 
-    @GetMapping
-    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompanyByName(@RequestBody DisplayRetailCompanyByNameRequest request){
+    @GetMapping("/getRetailsByName")
+    public ResponseEntity<List<? extends ResponseContract>> displayRetailCompanyByName(@RequestBody DisplayRetailCompanyByNameRequest request) {
         serviceHandler = "displayRetailCompanyByName";
         retailCompanyService.setDisplayRetailCompanyByNameRequest(request);
         var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
-        return  ResponseEntity.ok(response);
+        if (response.get(0) instanceof RetailCompanyResponse)
+            return ResponseEntity.ok(response);
+        else {
+            ErrorResponse error = (ErrorResponse) response;
+            logger.warn("Error response : {}", error);
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 
 
-    @GetMapping
-    public ResponseEntity<List<? extends ResponseContract>> displayAllRetailCompanies(@RequestBody DisplayRetailCompanyByNameRequest request){
+    @GetMapping("/getAllRetailCompanies")
+    public ResponseEntity<List<? extends ResponseContract>> displayAllRetailCompanies() {
         serviceHandler = "displayAllRetailCompanies";
-        var response = serviceConcurrentExecutor.buildServiceExecutor();
-        return  ResponseEntity.ok(response);
+        var response = serviceConcurrentExecutor.buildServiceExecutor(retailCompanyService);
+        if (response.get(0) instanceof RetailCompanyResponse)
+            return ResponseEntity.ok(response);
+        else {
+            ErrorResponse error = (ErrorResponse) response;
+            logger.warn("Error response : {}", error);
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 }
