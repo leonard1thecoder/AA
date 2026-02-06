e3d44eaae9afc82c2450359bb2cfd12900080cb7
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-06 09:34:25 +0200

    amend repository

--- privileges_microservice/src/main/java/com/privileges/application/repository/PrivilegesRepository.java
+++ privileges_microservice/src/main/java/com/privileges/application/repository/PrivilegesRepository.java
@@ -4,6 +4,10 @@ import com.privileges.application.entity.Privileges;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public interface PrivilegesRepository extends JpaRepository<Privileges,Integer> {
+    Optional<Privileges> findByPrivilegeName(String privilegeName);
 }
