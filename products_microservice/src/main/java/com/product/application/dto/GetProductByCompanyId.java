e0286c181b2f47d1fdd523578f42ce4d10c6588c
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-13 07:38:18 +0200

    amendments

---
+++ products_microservice/src/main/java/com/product/application/dto/GetProductByCompanyId.java
@@ -0,0 +1,11 @@
+package com.product.application.dto;
+
+import com.utils.application.RequestContract;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetProductByCompanyId implements RequestContract {
+    private Long companyId;
+}
