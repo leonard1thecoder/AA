46346339c00211b9834134eabf8914d7f13ee81e
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-17 15:38:04 +0200

    Amend the cart feature functionality

---
+++ cart_microservice/src/main/java/com/cart/application/dto/CheckoutRequest.java
@@ -0,0 +1,11 @@
+package com.cart.application.dto;
+
+import com.utils.application.RequestContract;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CheckoutRequest implements RequestContract {
+    private Long userId;
+}
