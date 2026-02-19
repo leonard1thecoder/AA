7312648631e99c47741118d2aea0c274cd8bc541
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-19 14:55:04 +0200

    Amend the cart feature functionality

--- payment_microservice/src/main/java/com/payment/application/controller/PaymentController.java
+++ payment_microservice/src/main/java/com/payment/application/controller/PaymentController.java
@@ -1,23 +1,40 @@
 package com.payment.application.controller;
 
-import com.payment.application.entity.Payment;
+import com.payment.application.dtos.PaymentRequest;
+import com.payment.application.dtos.PaymentResponseDTO;
+import com.payment.application.executor.PaymentServiceExecutor;
 import com.payment.application.service.PaymentService;
-import lombok.RequiredArgsConstructor;
+import com.utils.application.ResponseContract;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+
 @RestController
-@RequiredArgsConstructor
+
 @RequestMapping("/api/payments")
-public class PaymentController {
+public class PaymentController extends PaymentServiceExecutor {
 
     private final PaymentService paymentService;
 
+    public PaymentController(PaymentService paymentService) {
+        super(Executors.newVirtualThreadPerTaskExecutor());
+        this.paymentService = paymentService;
+    }
+
     @PostMapping("/process/{orderId}")
-    public ResponseEntity<Payment> processPayment(@PathVariable Long orderId) {
-        return ResponseEntity.ok(paymentService.pay(orderId,null));
+    public ResponseEntity<List<? extends ResponseContract>> processPayment(@PathVariable Long orderId, PaymentRequest request) {
+        request.setOrderId(orderId);
+        var response = super.executeService(paymentService, request, "pay");
+
+        if (response.getFirst() instanceof PaymentResponseDTO) {
+            return ResponseEntity.status(201).body(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
