b0731552be5f4162b518cdb44134cd756bccdd06
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-27 14:16:57 +0200

    amended controller

--- users_microservice/src/main/java/com/users/application/controllers/VerifyCustomerController.java
+++ users_microservice/src/main/java/com/users/application/controllers/VerifyCustomerController.java
@@ -1,45 +1,35 @@
 package com.users.application.controllers;
 
 
-import com.users.application.dtos.FindByEmailRequest;
 import com.users.application.dtos.FindByTokenRequest;
-import com.users.application.dtos.RollBackPasswordRequest;
 import com.users.application.dtos.UsersResponse;
-import com.users.application.executor.ServiceConcurrentExecutor;
+import com.users.application.executor.UserServiceConcurrentExecutor;
 import com.users.application.services.UsersService;
-import com.utils.application.globalExceptions.errorResponse.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.concurrent.Executors;
 
 @Controller
 @RequestMapping("/dev/api/verify")
-public class VerifyCustomerController {
+public class VerifyCustomerController extends UserServiceConcurrentExecutor{
 
     private final UsersService service;
-    private final ServiceConcurrentExecutor serviceConcurrentExecutor;
-
 
     @Autowired
     public VerifyCustomerController( @Autowired UsersService service) {
-        this.serviceConcurrentExecutor = ServiceConcurrentExecutor.getInstance();
-        this.service = service;
+       super(Executors.newVirtualThreadPerTaskExecutor());
+       this.service = service;
     }
 
     @GetMapping("/verify-customer")
     public String verifyCustomer(@RequestParam String qTokq1) {
-        FindByTokenRequest request = new FindByTokenRequest(qTokq1);
-        UsersService.setFindByTokenRequest(request);
-        UsersService.setServiceHandler("verifyUser");
-        var response = serviceConcurrentExecutor.buildServiceExecutor(service);
+        var response = super.executeUserService(service,"verifyUser",new FindByTokenRequest(qTokq1)).getFirst();
 
-        if(response.size() == 1){
+        if (response instanceof UsersResponse) {
             return "redirect:https://www.example.com";
         }else{
             return null;
@@ -49,10 +39,7 @@ public class VerifyCustomerController {
 
     @GetMapping("/verify-password-update")
     public String verifyPasswordUpdate(@RequestParam String qTokq1) {
-        FindByTokenRequest request = new FindByTokenRequest(qTokq1);
-        UsersService.setFindByTokenRequest(request);
-        UsersService.setServiceHandler("verifyUser");
-        var response = serviceConcurrentExecutor.buildServiceExecutor(service).get(0);
+        var response = super.executeUserService(service,"verifyUser",new FindByTokenRequest(qTokq1)).getFirst();
 
         if (response instanceof UsersResponse users) {
             return "redirect:/reset?emailAddess="+users.getUsersEmailAddress();
