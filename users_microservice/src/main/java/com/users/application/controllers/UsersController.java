e0028fc086a5ff13ec5d6f280079207d7571c2c4
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-02-27 14:01:35 +0200

    amend the controller methd to handle status codes

--- users_microservice/src/main/java/com/users/application/controllers/UsersController.java
+++ users_microservice/src/main/java/com/users/application/controllers/UsersController.java
@@ -3,77 +3,80 @@ package com.users.application.controllers;
 
 import com.users.application.dtos.*;
 
+import com.users.application.executor.UserServiceConcurrentExecutor;
 import com.users.application.services.UsersService;
 import com.utils.application.ResponseContract;
-import com.users.application.executor.ServiceConcurrentExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.utils.application.globalExceptions.errorResponse.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.List;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/dev/api/users")
-public class UsersController {
+public class UsersController extends UserServiceConcurrentExecutor {
     Logger logger = LoggerFactory.getLogger(UsersController.class);
 
     private final UsersService service;
-    private final ServiceConcurrentExecutor serviceConcurrentExecutor;
+    
 
 
     @Autowired
-
-    public UsersController( @Autowired UsersService service) {
-        this.serviceConcurrentExecutor = ServiceConcurrentExecutor.getInstance();
+    public UsersController(@Autowired UsersService service) {
+        super(Executors.newVirtualThreadPerTaskExecutor());
         this.service = service;
-
     }
 
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<? extends ResponseContract>> getAllUsers() {
-        UsersService.setServiceHandler("getAllUsers");
-        var list = this.serviceConcurrentExecutor.buildServiceExecutor(service);
+
+        var list = super.executeUserService(service, "getAllUsers", null);
         return ResponseEntity.ok(list);
     }
 
 
     @GetMapping("/getAllUsers/{id}")
     public ResponseEntity<List<? extends ResponseContract>> getUserById(@PathVariable Long id) {
-        UsersService.setServiceHandler("getUsersById");
-        service.setFindByIdRequest(new FindByIdRequest(id));
-        var list = this.serviceConcurrentExecutor.buildServiceExecutor(service).get(0);
- if (list instanceof UsersResponse users) {
+
+        var list = super.executeUserService(service, "getUsersById", new FindByIdRequest(id)).getFirst();
+        if (list instanceof UsersResponse users) {
             return ResponseEntity.ok(List.of(users));
         } else {
 
             ErrorResponse error = (ErrorResponse) list;
             logger.warn("Error response : {}", error);
-           return ResponseEntity.badRequest().body(List.of(error));
-        }  
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 
     @GetMapping("/getUsersByFullName/{fullName}")
     public ResponseEntity<List<? extends ResponseContract>> getUserByFullName(@PathVariable String fullName) {
-        UsersService.setServiceHandler("getUsersByFullName");
-        service.setUsersFullNameRequest(new UsersFullNameRequest(fullName));
-        var list = this.serviceConcurrentExecutor.buildServiceExecutor(service).get(0);
- if (list instanceof UsersResponse users) {
+        var list = super.executeUserService(service,"getUsersByFullName",new UsersFullNameRequest(fullName)).getFirst();
+        if (list instanceof UsersResponse users) {
             return ResponseEntity.ok(List.of(users));
         } else {
 
             ErrorResponse error = (ErrorResponse) list;
             logger.warn("Error response : {}", error);
-           return ResponseEntity.badRequest().body(List.of(error));
-        }    }
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
+    }
 
     @PostMapping("/findUserByIdentityNumber/{id}")
     public ResponseEntity<List<? extends ResponseContract>> getUserByIdNo(@RequestBody IdentityNoRequest request) {
-        UsersService.setServiceHandler("getUsersByIdentityNo");
-        service.setIdentityNoRequest(request);
-        var list = this.serviceConcurrentExecutor.buildServiceExecutor(service);
-        return ResponseEntity.ok(list);
+        var list = super.executeUserService(service,"getUsersByIdentityNo",request).getFirst();
+
+        if (list instanceof UsersResponse) {
+            return ResponseEntity.ok(List.of(list));
+        }else{
+            ErrorResponse error = (ErrorResponse) list;
+            logger.warn("Error response : {}", error);
+            return ResponseEntity.badRequest().body(List.of(error));
+        }
     }
 
 }
